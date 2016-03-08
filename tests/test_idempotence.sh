3cf7dff6a3d03ff5978aff66ee7e2877d9332490
Pedro Salgado <steenzout@saucelabs.com>
2016-03-08 09:19:07 -0700

    - default inventory changed to what Vagrant provisions - changed --extra-vars to -e - pass vagrant_box variable to the playbook - increased playbook verbosity when testing

--- tests/test_idempotence.sh
+++ tests/test_idempotence.sh
@@ -89,8 +89,8 @@ done
 
 # the name of the Vagrant box or host name
 BOX=${BOX:-localhost}
-# the Ansible inventory in the form of a file or string "host,"
-INVENTORY=${INVENTORY:-'localhost,'}
+# the Ansible inventory
+INVENTORY=${INVENTORY:-'.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory'}
 # the path to the Ansible test playbook
 PLAYBOOK=${PLAYBOOK:-test.yml}
 # the logfile to hold the output of the playbook run
@@ -98,13 +98,15 @@ LOGFILE="log/${BOX}_${VIRTUALENV_NAME}.log"
 
 EXTRA_ARGS=''
 if [ $BOX == "localhost" ]; then
-    EXTRA_ARGS="--connection=local --extra-vars idempotence=yes --extra-vars env=${ENV}"
+    INVENTORY='localhost,'
+    EXTRA_ARGS="--connection=local -e idempotence=yes -e env=${ENV} -e vagrant_box=localhost"
 else
-    EXTRA_ARGS="--u vagrant --extra-vars idempotence=yes --extra-vars env=vagrant"
+    EXTRA_ARGS="--user vagrant --e idempotence=yes -e env=vagrant -e vagrant_box=${BOX}"
 fi
 
 echo "[INFO] ${BOX} ${VIRTUALENV_NAME} running idempotence test..."
-ansible-playbook -i ${INVENTORY} --limit ${BOX}, ${EXTRA_ARGS} ${PLAYBOOK} 2>&1 | tee ${LOGFILE} | \
+ansible-playbook -vvvv -i ${INVENTORY} --limit ${BOX}, ${EXTRA_ARGS} ${PLAYBOOK} 2>&1 | \
+    tee ${LOGFILE} | \
     grep "${BOX}" | grep -q "${PASS_CRITERIA}" && \
     echo -ne "[TEST] ${BOX} ${VIRTUALENV_NAME} idempotence : ${GREEN}PASS${NC}\n" || \
     (echo -ne "[TEST] ${BOX} ${VIRTUALENV_NAME} idempotence : ${RED}FAILED${NC} ${PASS_CRITERIA}\n" && cat ${LOGFILE} && exit 1)
