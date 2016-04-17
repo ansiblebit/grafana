f7d4d7161e23f2b5c2621440f61705e5a8804aa7
Pedro Salgado <steenzout@ymail.com>
2016-04-17 17:09:58 -0600

    log file now test dependent, tweaked EXTRA_ARGS, idempotence is now an environment variable, failed tests now print log file and other minor changes

--- tests/test_idempotence.sh
+++ tests/test_idempotence.sh
@@ -23,7 +23,7 @@
 #
 #   # on a Vagrant box
 #   bash test_idempotence.sh \
-#       --box precise64.vagrant.dev
+#       --box precise64.vagrant.dev \
 #       --inventory .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory
 #
 #
@@ -50,7 +50,7 @@ RED='\033[0;31m'
 # SGR code to set text color (foreground) to no color.
 NC='\033[0m'
 # The idempotence pass criteria.
-PASS_CRITERIA="changed=0.*unreachable=0.*failed=0"
+PASS_CRITERIA="changed=0.* unreachable=0.* failed=0"
 
 # the name of the virtualenv
 VIRTUALENV_NAME=$(which python | awk -F / 'NF && NF-2 { print ( $(NF-2) ) }')
@@ -97,19 +97,23 @@ INVENTORY=${INVENTORY:-'.vagrant/provisioners/ansible/inventory/vagrant_ansible_
 # the path to the Ansible test playbook
 PLAYBOOK=${PLAYBOOK:-test.yml}
 # the logfile to hold the output of the playbook run
-LOGFILE="log/${BOX}_${VIRTUALENV_NAME}.log"
+LOGFILE="log/${BOX}_idempotence_${VIRTUALENV_NAME}.log"
 
 EXTRA_ARGS=''
 if [ $BOX == "localhost" ]; then
     INVENTORY='localhost,'
-    EXTRA_ARGS="--connection=local -e idempotence=yes -e env=${ENV} -e vagrant_box=localhost"
+    EXTRA_ARGS="--connection=local -e env=${ENV} -e vagrant_box=localhost"
 else
-    EXTRA_ARGS="--user vagrant --e idempotence=yes -e env=vagrant -e vagrant_box=${BOX}"
+    EXTRA_ARGS="--user vagrant -e env=vagrant -e vagrant_box=${BOX}"
 fi
 
 echo "[INFO] ${BOX} ${VIRTUALENV_NAME} running idempotence test..."
-ansible-playbook -vvvv -i ${INVENTORY} --limit ${BOX}, ${EXTRA_ARGS} ${PLAYBOOK} 2>&1 | \
+IDEMPOTENCE='yes' \
+    ansible-playbook -vvvv -i ${INVENTORY} --limit ${BOX}, ${EXTRA_ARGS} ${PLAYBOOK} 2>&1 | \
     tee ${LOGFILE} | \
     grep "${BOX}" | grep -q "${PASS_CRITERIA}" && \
-    echo -ne "[TEST] ${BOX} ${VIRTUALENV_NAME} idempotence : ${GREEN}PASS${NC}\n" || \
-    (echo -ne "[TEST] ${BOX} ${VIRTUALENV_NAME} idempotence : ${RED}FAILED${NC} ${PASS_CRITERIA}\n" && cat ${LOGFILE} && exit 1)
+    echo -ne "[TEST] ${BOX} ${VIRTUALENV_NAME} idempotence : ${GREEN}PASS${NC}\n" || ( \
+        cat ${LOGFILE} &&
+        echo -ne "[TEST] ${BOX} ${VIRTUALENV_NAME} idempotence : ${RED}FAILED${NC} ${PASS_CRITERIA}\n" && \
+        exit 1)
+
