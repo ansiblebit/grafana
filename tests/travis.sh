3f570ecda009bfccb4de49d1829dcdd1e5fc44b0
Pedro Salgado <steenzout@ymail.com>
2016-06-10 12:59:58 -0600

    prevent travis-ci to run vagrant.sh when tests fail

--- tests/travis.sh
+++ tests/travis.sh
@@ -23,15 +23,17 @@
 #
 # #################
 
-test $USER = 'travis' \
-    && ansible-playbook \
-        -i localhost, \
-        --connection=local test.yml \
-        -e vagrant_box=localhost \
-        -e env=travis \
-        --skip-tags=test \
-        $@ \
-    && bash test_checkmode.sh \
-        --env travis \
-    && bash test_idempotence.sh \
-        --env travis
+set -e
+
+ansible-playbook \
+    -i localhost, \
+    --connection=local test.yml \
+    -e vagrant_box=localhost \
+    -e env=travis \
+    --skip-tags=test \
+    $@ \
+&& bash test_checkmode.sh \
+    --env travis \
+&& bash test_idempotence.sh \
+    --env travis
+
